f10a34ace960696eb0ba3d718be824acae80bfd7
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-06-03 01:39:21 +0800

    feat: route groups organizing routes

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Register</h1>
+            <p>Please fill out the form to create a new account.</p>
+            {/* Add your registration form or components here */}
+            
+        </div>
+    );
+}
---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <p>Please enter your credentials to sign in.</p>
+            {/* Add your sign-in form or components here */}
+        </div>
+    );
+}
