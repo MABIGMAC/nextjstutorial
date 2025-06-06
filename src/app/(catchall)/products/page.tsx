1bd9e37434ffcdeb466aaad76e475f5bdf0daa0e
McBigMac <jy1996@hotmail.com>
2025-06-06 17:31:27 +0800

    feat: Linking Component Navigation

--- src/app/(catchall)/products/page.tsx
+++ src/app/(catchall)/products/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import Link from "next/link"
 
 export const metadata: Metadata = {
     title: {
@@ -6,9 +7,20 @@ export const metadata: Metadata = {
 
 export default function Product() {
     return <>
+    <Link href="/">
+    Home
+    </Link>
     <h1>Product Page</h1>;
-    <h2>Product 1</h2>
-    <h2>Product 2</h2>
-    <h2>Product 3</h2>
+    
+    <h2>
+        <Link href="/products/1">
+        Product 1
+        </Link>
+    </h2>
+    <h2>
+        <Link href="/products/2">
+        Product 2
+        </Link>
+    </h2>
     </> 
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-    return <h1>Hello World@</h1>
+    return (<>
+    <h1>Hello World@</h1>
+    <Link href="/about">
+    Go to About Page
+    </Link>
+    <Link href="/products">
+    Go to Products Page
+    </Link>
+    </>)
 }
