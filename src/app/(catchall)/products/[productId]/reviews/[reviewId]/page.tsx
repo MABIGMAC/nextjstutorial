656dd82d6c54777f70485a11c9c2cb0ee6efc65e
McBigMac <jy1996@hotmail.com>
2025-06-09 15:29:13 +0800

    feat: Error Handling

--- src/app/(catchall)/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/(catchall)/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,16 @@
 import { notFound, redirect } from "next/navigation";
 
+function getRandomInt(max: number): number {
+    return Math.floor(Math.random() * max);
+}
+
 export default async function review({params}: {params: Promise<{
     productId: string,
     reviewId: number}> }) {
+    const randomNumber = getRandomInt(2);
+    if (randomNumber === 1) {
+        throw new Error("An error occurred while fetching the review details.");
+    }
     const { productId ,reviewId } = await params;
     if (reviewId > 1000) {                                                                                                                                                                                                                
         redirect('/products')                                                                                                                                                                                                                 
