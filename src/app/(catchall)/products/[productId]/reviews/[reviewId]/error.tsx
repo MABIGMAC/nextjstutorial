6b92d5232b9d0ab707d2e748e73da2c03aeefe80
McBigMac <jy1996@hotmail.com>
2025-06-09 15:34:32 +0800

    feat: recovering from error

--- src/app/(catchall)/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/(catchall)/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,19 @@
 "use client"
-export default function ErrorBoundary({ error }: { error: Error }) {
+
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function ErrorBoundary({ error, reset }: { error: Error; reset: () => void }) {
+    const router = useRouter();
+    const reload = () => {
+        startTransition(() => {
+            // This will reload the current route
+            router.refresh();
+            reset();
+        })
+    }
     return <div>
         Error in reviewId {error.message}
+        <button onClick={() => reload()}>Try again</button>
     </div>
 }
