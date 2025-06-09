955a5fd6dd0950b7b0454db92280fba3b41e3041
McBigMac <jy1996@hotmail.com>
2025-06-09 15:09:07 +0800

    feat: loading states

--- src/app/(catchall)/blog/page.tsx
+++ src/app/(catchall)/blog/page.tsx
@@ -2,6 +2,8 @@ export const metadata = {
     title: 'Blog Pages',
     description: 'This is the blog page of the application.',}
 
-export default function Blog() {
+export default async function Blog() {
+    await new Promise(resolve => setTimeout(resolve, 2000)); // Simulate a delay
+
     return <h1>Blog Page</h1>;
 }
