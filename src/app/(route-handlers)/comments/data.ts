a44d2524ac442ca5fbfe90c3d6065eeeb487826e
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-06-10 22:36:57 +0800

    feat: get request

---
+++ src/app/(route-handlers)/comments/data.ts
@@ -0,0 +1,16 @@
+import { text } from "stream/consumers";
+
+export const comments = [
+    {
+        id: 1,
+        text: "This is the first comment."
+    },
+    {
+        id: 2,
+        text: "This is the second comment."
+    },
+    {
+        id: 3,
+        text: "This is the third comment."
+    },
+]
