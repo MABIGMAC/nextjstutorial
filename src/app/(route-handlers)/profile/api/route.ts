216c862548153d145c4db381f765a4e8ceb487f9
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-06-19 22:03:17 +0800

    feat: cookies in request

--- src/app/(route-handlers)/profile/api/route.ts
+++ src/app/(route-handlers)/profile/api/route.ts
@@ -1,13 +1,19 @@
+import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers)
   console.log("Request Headers:", requestHeaders);
   console.log("the auth token is:", requestHeaders.get("authorization"));
+  const theme = request.cookies.get("theme")?.value || "light"; // Default to 'light' if not set
+  console.log("Theme:", theme);
+  const cookieStore = await cookies();
+  cookieStore.set("results per Page", "10");
+
   return new Response("Hello, profile api!", {
     headers: {
       "Content-Type": "text/html",
-      "Cache-Control": "no-store",
+      "Set-Cookie": "theme=dark"
     },
   });
 }
