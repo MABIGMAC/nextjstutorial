14c7d23a98dd49f99ab4f0f4e0624e35aa20c497
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-06-19 21:25:17 +0800

    feat: dynamic route handlers

---
+++ src/app/(route-handlers)/comments/[id]/route.tsx
@@ -0,0 +1,7 @@
+import { comments } from "../data";
+
+export async function GET(request: Request, { params}: { params: Promise<{id: string}> } ) {
+    const { id } = await params;
+    const comment = comments.find(c => c.id === parseInt(id));
+    return Response.json(comment || { error: "Comment not found" })
+}
