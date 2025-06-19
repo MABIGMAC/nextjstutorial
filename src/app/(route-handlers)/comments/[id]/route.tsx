577c5af9fa9095d449a4f1f6420b6b9b47cf665c
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-06-19 21:36:46 +0800

    feat: delete request

--- src/app/(route-handlers)/comments/[id]/route.tsx
+++ src/app/(route-handlers)/comments/[id]/route.tsx
@@ -16,3 +16,13 @@ export async function PATCH(request: Request, { params }: { params: Promise<{id:
     comment.text = text;
     return Response.json(comment);
 }
+
+export async function DELETE(request: Request, { params }: { params: Promise<{id: string}> }) {
+    const { id } = await params;
+    const index = comments.findIndex(c => c.id === parseInt(id));
+    if (index === -1) {
+        return Response.json({ error: "Comment not found" }, { status: 404 });
+    }
+    comments.splice(index, 1);
+    return Response.json({ message: "Comment deleted successfully" });
+}
