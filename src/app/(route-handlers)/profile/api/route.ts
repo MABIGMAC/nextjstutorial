226107f09f3c4e8dd5c5e4d1090c5face54f8462
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-06-19 21:53:35 +0800

    feat: headers in request

--- src/app/(route-handlers)/profile/api/route.ts
+++ src/app/(route-handlers)/profile/api/route.ts
@@ -1,3 +1,14 @@
-export async function GET() {
-  return new Response("Hello, profile api!");
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers)
+  console.log("Request Headers:", requestHeaders);
+  console.log("the auth token is:", requestHeaders.get("authorization"));
+  return new Response("Hello, profile api!", {
+    headers: {
+      "Content-Type": "text/html",
+      "Cache-Control": "no-store",
+    },
+  });
 }
+
