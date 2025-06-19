cd107d15abbfe77d149908da4dc09526a7edff3f
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-06-19 22:52:45 +0800

    feat: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+export function middleware(request: NextRequest){
+    const response = NextResponse.next();
+    const themePreference = request.cookies.get("theme")
+    if (!themePreference) {
+        response.cookies.set("theme", "dark")
+    }
+
+    response.headers.set("X-Custom-Header", "Hello from middleware");
+
+    return response;
+
+
+    if (request.nextUrl.pathname === "/products") {
+        return NextResponse.redirect(new URL("/", request.nextUrl));
+    }
+
+    // return NextResponse.redirect(new URL("/", request.url));
+}
+
+// export const config = {
+//     matcher: ["/profile", "/profile/:path*"],
+// }
