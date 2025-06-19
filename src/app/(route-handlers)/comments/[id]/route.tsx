95c4838f9eb683999a6846f896ac1dfdda65723e
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-06-19 21:33:42 +0800

    feat: patch request

--- src/app/(route-handlers)/comments/[id]/route.tsx
+++ src/app/(route-handlers)/comments/[id]/route.tsx
@@ -5,3 +5,14 @@ export async function GET(request: Request, { params}: { params: Promise<{id: st
     const comment = comments.find(c => c.id === parseInt(id));
     return Response.json(comment || { error: "Comment not found" })
 }
+
+export async function PATCH(request: Request, { params }: { params: Promise<{id: string}> }) {
+    const { id } = await params;
+    const comment = comments.find(c => c.id === parseInt(id));
+    if (!comment) {
+        return Response.json({ error: "Comment not found" }, { status: 404 });
+    }
+    const { text } = await request.json();
+    comment.text = text;
+    return Response.json(comment);
+}
