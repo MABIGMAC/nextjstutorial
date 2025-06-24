c0d2fc5026e36bb2fc5d93fd97f50ad0c35c3ea2
McBigMac <jy1996@hotmail.com>
2025-06-24 17:02:19 +0800

    feat: client only code

--- src/app/(server-client-seperation)/client-page/page.tsx
+++ src/app/(server-client-seperation)/client-page/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { useTheme } from "@/components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 export default function ClientPage() {
     const { colors} = useTheme();
+    const result = clientSideFunction();
     return (
         <div>
             <h1 style={{ color: colors.secondary }}>Client Page</h1>
-            Client Page 
+            {result}
         </div>
     )
 }
---
+++ src/utils/client-utils.ts
@@ -0,0 +1,5 @@
+import "client-only"
+export const clientSideFunction = () => {
+    console.log("This function runs on the client side.");
+    return "Client-side function executed successfully.";
+    }   
