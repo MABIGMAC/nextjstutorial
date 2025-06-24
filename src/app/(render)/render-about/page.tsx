ba47b8fc9fa61e484a83c07b14416f3bb24589ad
McBigMac <jy1996@hotmail.com>
2025-06-24 12:50:53 +0800

    feat: dynamic generation when using cookies

--- src/app/(render)/render-about/page.tsx
+++ src/app/(render)/render-about/page.tsx
@@ -1,4 +1,9 @@
-export default function AboutPage() {
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
+    const cookieStore = await cookies();
+    const theme = cookieStore.get("theme")
+    console.log("Current theme from cookies:", theme);
     console.log("AboutPage rendered");
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 export default function Home() {
     return (<>
     <h1>Hello World@</h1>
-    <Link href="/about">
-    Go to About Page
+    <Link href="/render-about">
+    render about
+    </Link>
+    <Link href="/render-dashboard">
+    render dashboard
     </Link>
     <Link href="/products">Go to Products Page</Link>
     <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
