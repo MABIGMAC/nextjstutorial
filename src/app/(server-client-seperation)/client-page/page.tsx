9d503f4db0d59a8d8be309cc11c5685335391bc8
McBigMac <jy1996@hotmail.com>
2025-06-24 16:58:09 +0800

    feat: context provider

--- src/app/(server-client-seperation)/client-page/page.tsx
+++ src/app/(server-client-seperation)/client-page/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { serverSideFunction } from "@/utils/server-utils";
 
-const serverResponse = serverSideFunction();
+import { useTheme } from "@/components/theme-provider";
+
 export default function ClientPage() {
+    const { colors} = useTheme();
     return (
         <div>
-            Client Page {serverResponse}
+            <h1 style={{ color: colors.secondary }}>Client Page</h1>
+            Client Page 
         </div>
     )
 }
