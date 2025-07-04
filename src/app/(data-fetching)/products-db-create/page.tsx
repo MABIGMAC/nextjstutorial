3fc37b6369b46d5faec1fda7521532675f889c92
MABIGMAC <jy1996@hotmail.com>
2025-07-04 18:15:58 +0800

    feat: seperating server actions

--- src/app/(data-fetching)/products-db-create/page.tsx
+++ src/app/(data-fetching)/products-db-create/page.tsx
@@ -1,18 +1,8 @@
 "use client";
-import SubmitButton from "@/components/submit-button";
-import { createProduct } from "@/prisma-db";
-import { redirect } from "next/navigation";
+import { FormState, createProductButton } from "@/actions/products";
 import { useActionState } from "react";
 
-type Errors = {
-    title?: string;
-    price?: string;
-    description?: string;
-}
 
-type FormState = {
-    errors: Errors;
-}
 
 export default function AddProductPage() {
     const initialState: FormState = {
@@ -20,30 +10,6 @@ export default function AddProductPage() {
     };
 
     const [state, formAction, isPending]= useActionState(createProductButton,initialState)
-    async function createProductButton(formData: FormData){
-        "use server"
-        const title = formData.get("title") as string;
-        const price = formData.get("price") as string;
-        const description = formData.get("description") as string;
-
-        const errors: Errors = {};
-        if (!title) {
-            errors.title = "Title is required";
-        }
-        if (!price || isNaN(parseFloat(price))) {
-            errors.price = "Price must be a valid number";
-        }
-        if (!description) {
-            errors.description = "Description is required";
-        }
-
-        if (Object.keys(errors).length > 0) {
-            return { errors };
-        }
-
-        await createProduct(title, parseInt(price), description);
-        redirect("/products-db");
-    }
     
     return (
         <form action={formAction} className="max-w-md mx-auto p-4 bg-white shadow-md rounded-lg">
@@ -53,7 +19,6 @@ export default function AddProductPage() {
                 <input
                     type="text"
                     className="w-full p-2 border border-gray-300 rounded"
-                    required
                     name="title"
                 />
             {
@@ -69,7 +34,6 @@ export default function AddProductPage() {
                 <input
                     type="number"
                     className="w-full p-2 border border-gray-300 rounded"
-                    required
                     name="price"
                 />
             {
