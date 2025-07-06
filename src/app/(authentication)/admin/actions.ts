9d167ad7f7d03fa930486fc7c5ea4e2ffae0a36d
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-07-06 18:13:12 +0800

    chore: admin feature add role

---
+++ src/app/(authentication)/admin/actions.ts
@@ -0,0 +1,51 @@
+"use server"
+
+import { auth, clerkClient } from "@clerk/nextjs/server"
+import { Roles } from "../../../../types/globals"
+import { revalidatePath } from "next/cache"
+
+export async function setRole(formData: FormData) {
+    const {sessionClaims}= await auth()
+    if (sessionClaims?.metadata?.role !== "admin") {
+        throw new Error("You are not authorized to perform this action")
+    }
+
+    const client = await clerkClient()
+    const id = formData.get("id") as string
+    const role = formData.get("role") as Roles
+
+    try {
+        await client.users.updateUser(id, {
+            publicMetadata: {
+                role: role
+            }
+        })
+        revalidatePath("/admin")
+
+    } catch (error) {
+        console.error("Error updating user role:", error)
+        throw new Error("Failed to update user role")
+    }
+}
+
+export async function removeRole(formData: FormData) {
+    const {sessionClaims}= await auth()
+    if (sessionClaims?.metadata?.role !== "admin") {
+        throw new Error("You are not authorized to perform this action")
+    }
+
+    const client = await clerkClient()
+    const id = formData.get("id") as string
+
+    try {
+        await client.users.updateUser(id, {
+            publicMetadata: {
+                role: undefined
+            }
+        })
+        revalidatePath("/admin")
+    } catch (error) {
+        console.error("Error removing user role:", error)
+        throw new Error("Failed to remove user role")
+    }
+}
--- src/app/(authentication)/admin/page.tsx
+++ src/app/(authentication)/admin/page.tsx
@@ -1,3 +1,36 @@
-export default function Admin() {
-    return <h1> Admin only Page </h1>
+import { clerkClient } from "@clerk/nextjs/server";
+import { removeRole, setRole } from "./actions";
+
+export default async function Admin() {
+    const client = await clerkClient();
+    const users = (await client.users.getUserList()).data
+    return (
+        <>
+            {
+                users.map((user) => {
+                    return (
+                        <div key={user.id} className="flex flex-col items-center justify-center p-4 border rounded-lg shadow-md m-2 ${users.indexOf(user) % 2 === 0 ? 'bg-gray-300' : 'bg-white'}">
+                            <div className="text-lg font-semibold mb-2">{user.firstName} {user.lastName}</div>
+                            <div className="text-sm text-gray-600 mb-2">Email: {user.emailAddresses[0]?.emailAddress}</div>
+                            <div className="text-sm text-gray-600 mb-2">Role: {user.publicMetadata.role as string}</div>
+                            <form action={setRole} className="flex flex-col items-center">
+                                <input type="hidden" name="id" value={user.id} />
+                                <select name="role" className="mb-2 p-1 border rounded">
+                                    <option value="user">User</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="super-admin">Super Admin</option>
+                                </select>
+                                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-2">Set Role</button>
+                                </form>
+                                <form action={removeRole} className="flex flex-col items-center">
+                                    <input type="hidden" name="id" value={user.id} />
+                                    <button type="submit" className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">Remove Role</button>
+                                </form>
+                        </div>
+                    )
+                })
+
+            }
+        </>
+    )
 }
